cdc752a0081fe1a0f0573cb89d45a36661c8c7b7
Ketan Shah <ketan.shah@gmail.com>
2016-05-03 15:50:04 +0530

    grouping faqs by category

--- src/templates/mrk_divi_widget_faq_post_type.blade.php
+++ src/templates/mrk_divi_widget_faq_post_type.blade.php
@@ -4,16 +4,7 @@
      * @var string
      */
     $custom_post_type = 'faq';
-?>
-
-    <?php
-
-    $i = 0;
-    //Query events according to shortcode
-    $eventargs = array(
-            'post_type'      => $custom_post_type,
-            'posts_per_page' => -1,
-    );
+    $i                = 0;
 
     $terms_args = array(
         'orderby' => 'name',
@@ -21,31 +12,14 @@
     );
 
     if ( !is_null($include_categories) || $include_categories ) {
-        $included_terms         = explode( ',', $include_categories );
-        $terms_args['include']  = $included_terms;
-
-        $eventargs['tax_query'] = array(
-            array(
-                'taxonomy' => 'faq_category',
-                'field'    => 'id',
-                'terms'    => $included_terms,
-                'operator' => 'IN',
-            ),
-        );
+        $terms_args['include']  = $include_categories;
     }
 
     $terms = get_terms( 'faq_category', $terms_args );
 
-    global $wp_query;
-
-    $temp_query = $wp_query;
-
-    $wp_query = new WP_Query($eventargs);
-
     ?>
 
-
-    @if($enable_search == 'on')
+   @if($enable_search == 'on')
     <div class="et_pb_search et_pb_module et_pb_bg_layout_light et_pb_text_align_left  et_pb_search_0">
         <form role="search" method="get" class="et_pb_searchform" action="/" _lpchecked="1">
             <div>
@@ -58,21 +32,34 @@
     </div>
     @endif
 
-
-
-
     @foreach($terms as $term)
         <?php
             $shortcode = array();
 
-            $shortcode[] = '[et_pb_section admin_label="section"][et_pb_row admin_label="row"]';
-            $shorcode[]  = sprintf('[et_pb_section fullwidth="off" specialty="off" admin_label="Section"][et_pb_row admin_label="Row"][et_pb_column type="4_4"][et_pb_text admin_label="Text" background_layout="light" text_orientation="center" use_border_color="off" border_color="#ffffff" border_style="solid"]%s[/et_pb_text][et_pb_toggle admin_label="Toggle" title="Hello" open="off" use_border_color="off" border_color="#ffffff" border_style="solid"]World[/et_pb_toggle][/et_pb_column][/et_pb_row][/et_pb_section]', 'hhel');
+            $shortcode[]  = sprintf('[et_pb_section fullwidth="off" specialty="off" admin_label="Section" custom_padding="0|0|0|0"][et_pb_row admin_label="Row"][et_pb_column type="4_4" custom_padding="0|0|0|0"][et_pb_text admin_label="Text" background_layout="light" text_orientation="left" use_border_color="off" border_color="#ffffff" border_style="solid"]%s[/et_pb_text]', $term->name);
+
+            $args = array(
+                    'post_type' => $custom_post_type,
+                    'tax_query' => array(
+                        array(
+                        'taxonomy' => 'faq_category',
+                        'field'    => 'slug',
+                        'terms'    => $term->slug,
+                         ),
+                      ),
+            );
+
+            $query = new WP_Query( $args );
+
+            while ($query->have_posts()) {
+                $query->the_post();
+
+                $shortcode[] = sprintf('[et_pb_toggle admin_label="Toggle" title="%s" open="off" use_border_color="off" border_color="#ffffff" border_style="solid"]%s[/et_pb_toggle]', get_the_title(), get_the_content());
+            }
+
+            $shortcode[] = '[/et_pb_column][/et_pb_row][/et_pb_section]';
 
         ?>
         {{ do_shortcode(implode('', $shortcode)) }}
 
 @endforeach
-
-
-
-
